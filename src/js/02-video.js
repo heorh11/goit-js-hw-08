69fcda89b1ebc79cca229f4bf6d1a30cffae5ce5
heorh11 <gheorghe.filip225@gmail.com>
2023-09-17 16:06:42 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,22 +5,18 @@ const vimeoPlayer = new Player('vimeo-player'); // 'vimeo-player' - це ID ва
 
 const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
-// Отримуємо збережений час відтворення з локального сховища
-const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-// Відтворюємо відео та встановлюємо час відтворення
-vimeoPlayer.play().then(() => {
-  if (savedTime) {
-    vimeoPlayer.setCurrentTime(parseFloat(savedTime));
-  }
-
-  // Встановлюємо обробник події timeupdate для відстежування часу відтворення
-  vimeoPlayer.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
-});
+// Встановлюємо обробник події timeupdate для відстежування часу відтворення
+vimeoPlayer.on('timeupdate', throttle(saveTimeToLocalStorage, 1000));
 
 // Функція для зберігання часу відтворення в локальному сховищі
 function saveTimeToLocalStorage(event) {
   const currentTime = event.seconds.toFixed(2);
   localStorage.setItem(LOCAL_STORAGE_KEY, currentTime);
+}
+
+// Отримуємо збережений час відтворення з локального сховища та встановлюємо час відтворення
+const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedTime !== null) {
+  vimeoPlayer.setCurrentTime(parseFloat(savedTime));
 }
 
